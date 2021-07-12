d3b5a1517b8400264675e69d176df6f9a157d098
mvp-11235 <84168388+mvp-11235@users.noreply.github.com>
2021-07-12 21:40:25 +0900

    Random background creator

---
+++ settings/js/randomBackground.js
@@ -0,0 +1,4 @@
+const body = document.querySelector("body");
+let background = ["#ffb3ba", "#ffdfba", "#ffffba", "#baffc9", "#bae1ff"]
+let random = Math.random() * background.length;
+body.style.backgroundColor = background[Math.floor(random)];
