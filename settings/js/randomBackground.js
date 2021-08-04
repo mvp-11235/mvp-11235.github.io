8bd7c1bc52241fefa96729816191e1df7454759f
mvp-11235_dev <84168388+mvp-11235@users.noreply.github.com>
2021-08-04 11:54:22 +0900

    Delete randomBackground.js

--- settings/js/randomBackground.js
+++
@@ -1,5 +0,0 @@
-const body = document.querySelector("body");
-let background = ["#ffb3ba", "#ffdfba", "#ffffba", "#baffc9", "#bae1ff"]
-let random = Math.random() * background.length;
-
-body.style.backgroundColor = background[Math.floor(random)];
